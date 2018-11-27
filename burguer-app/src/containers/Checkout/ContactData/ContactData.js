1c8e234a3fde6c78ab7f1796002aca32ed9c76f5
vinicius p vieira <vinipachecov@gmail.com>
2018-11-26 23:42:22 -0200

    Forms
    Added form based on state of the ContactData
    class. The input props are hard coded in the
    state of the class which passes the props
    to the function component where it
    renders it appropriately.
    
    Hard coded inputs have been replaced by
    the custom stateless input component and it's classes.

--- burguer-app/src/containers/Checkout/ContactData/ContactData.js
+++ burguer-app/src/containers/Checkout/ContactData/ContactData.js
@@ -3,15 +3,62 @@ import React, { Component } from 'react'
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Button from '../../../components/UI/Button/Button';
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.css'
 
 class ContactData extends Component {
   state = {    
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Zipcode'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your E-mail'
+        },
+        value: ''
+      }, 
+      email: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest'},
+            { value: 'cheapest', displayValue: 'Cheapest'}
+        ]
+        },
+        value: ''
+      },                     
     },
     loading: false
   }  
@@ -45,12 +92,23 @@ class ContactData extends Component {
 
 
   render() {
+    const formElementsArray = [];
+    for (const key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      })           
+    }
     const { loading } = this.state;
-    let form = ( <form>
-      <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-      <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-      <input className={classes.Input} type="text" name="street" placeholder="Street" />
-      <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />          
+    let form = ( <form>      
+      { formElementsArray.map(formElement => (
+        <Input           
+          key={formElement.id}
+          elementType={formElement.config.elementType}
+          elementConfig={formElement.config.elementConfig}
+          value={formElement.config.value}
+        />
+      ))}      
       <Button btnType="Success" clicked={this.orderHandler} >ORDER</Button>
     </form> );
     if (loading) {
